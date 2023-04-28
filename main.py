f62a20bb8d77765aa5d0ff3e6bfba8d134c47c8b
Vagif <np.info.mail@gmail.com>
2023-04-28 17:42:09 +0400

    first commit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request, jsonify
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
+upload_folder = 'uploads'
+app.config['UPLOAD_FOLDER'] = upload_folder
 app.secret_key = "marangozum"
 
 ### index ŞEHİR İLÇE BİLGİSİ İSTER
@@ -14,9 +16,19 @@ def home():
 
 ### TÜM BİLGİLERİ İSTER ŞEHİR İLÇEYİ İŞLER
 
-@app.route('/form')
-def info():
-    print(request.form)
+@app.route('/upload')
+def upload():
+    if request.method == 'POST':
+
+        # Get the list of files from webpage
+        files = request.files.getlist("file")
+
+        # Iterate for each file in the files List, and Save them
+        for file in files:
+            path = "{upf}/{fn}".format(upf=upload_folder, fn=file.filename)
+            file.save(path)
+        print(request.form)
+        return "<h1>Files Uploaded Successfully.!</h1>"
 
     return render_template('info.html')
 
