94f29422a6820fb3750cc08fe88ab667633354e1
Vagif <np.info.mail@gmail.com>
2023-05-03 14:33:54 +0400

    last commit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
+import random
+import string
+
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 upload_folder = 'uploads'
@@ -10,27 +13,45 @@ app.secret_key = "marangozum"
 
 @app.route('/')
 def home():
-    print({"ip_1": request.remote_addr})
+    # print({"ip_1": request.remote_addr})
     return render_template('index.html')
 
 
 ### TÜM BİLGİLERİ İSTER ŞEHİR İLÇEYİ İŞLER
 
-@app.route('/upload')
+@app.route('/upload', methods=['POST'])
 def upload():
     if request.method == 'POST':
 
         # Get the list of files from webpage
-        files = request.files.getlist("file")
+        files = request.files.getlist("myFiles")
 
         # Iterate for each file in the files List, and Save them
         for file in files:
-            path = "{upf}/{fn}".format(upf=upload_folder, fn=file.filename)
+            extension = str(file.filename).split(".")[-1]
+            filename = "{fn}.{ex}".format(fn=''.join(random.choices(string.ascii_letters, k=16)), ex=extension)
+            path = "{upf}/{fn}".format(upf=upload_folder, fn=filename)
             file.save(path)
-        print(request.form)
-        return "<h1>Files Uploaded Successfully.!</h1>"
+        city = request.form.get('city')
+        dist = request.form.get('dist')
+        brand = request.form.get('brand')
 
-    return render_template('info.html')
+        return_data = {'city': city, 'dist': dist, 'brand': brand, 'totalFiles': len(files)}
+        print(return_data)
+        return "Uploaded successfully!"
+
+    return "Not supported method!"
+
+
+@app.route('/info')
+def info():
+    brand = request.args['brand']
+    city = request.args['city']
+    dist = request.args['dist']
+    total_files = request.args['totalFiles']
+    data = {'brand': brand, 'city': city, 'dist': dist, 'totalFiles': total_files}
+    print('CALLED from here')
+    return render_template('result.html', data=data)
 
 
 if __name__ == '__main__':
